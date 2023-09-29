ded2ced667cb8567ab14e91aaeea94ac6494b6a5
SamvelYesayan <samyesayan6@gmail.com>
2023-09-29 12:44:49 +0400

    Home Page First Component

---
+++ src/components/SectionHome1/SectionHome1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SectionHome1.css'
+
+
+const SectionHome1 = () => {
+  return (
+    <div className='home-sect1-div'>
+        
+            <b>Explore the Colourful World</b>
+            <hr />
+            <h1>A WONDERFUL GIFT</h1>
+            <button>LEARN MORE</button>
+        
+    </div>
+  )
+}
+
+export default SectionHome1
