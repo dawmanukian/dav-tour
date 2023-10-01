0242f62830fd76c87d290c3fed612900cffa076b
SamvelYesayan <samyesayan6@gmail.com>
2023-10-01 15:59:29 +0400

    test

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SectionHome1 from '../../components/SectionHome1/SectionHome1'
 import SectionHome2 from '../../components/SectionHome2/SectionHome2'
+import SectionHome3 from '../../components/SectionHome3/SectionHome3'
 const HomePage = () => {
   return (
     <div>
         <SectionHome1 />
-        <SectionHome2 />
+        <SectionHome2 /> 
+        <SectionHome3 />
     </div>
   )
 }
