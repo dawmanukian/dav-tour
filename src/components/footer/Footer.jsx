25c30ee82e4cfc63780413a471ad3687dccb9d72
dawitmanukian <davit.manukyan.d@tumo.org>
2023-10-02 20:04:00 +0400

    footer updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,14 +6,17 @@ const Footer = () => {
   return (
     <footer>
         <div>          
-          <p>Address: Sevan, Armenia | Phone: 800-123-456 | Email: davtour@gmail.com</p>
+          <h5>Address: Sevan, Armenia | Phone: 800-123-456 | Email: davtour@gmail.com</h5>
         </div>
         <b>Copyright © 2023 Dav Tour Agency</b>
         <div className="footer-social-links">
             <a href='https://www.instagram.com/davv_tourr/' target='_blank' rel='noreferrer'><FaFacebookSquare /></a>
             <a href='https://www.instagram.com/davv_tourr/' target='_blank' rel='noreferrer'><FaInstagram /></a>
             <a href='https://www.instagram.com/davv_tourr/' target='_blank' rel='noreferrer'><FaLinkedin /></a>
-        </div>      
+        </div>
+        <div>
+          <p className='magical'>Created by <a href='#' target='_blank' rel='noreferrer' className='magical-site'>Magical Company</a></p>
+        </div>
     </footer>
   )
 }
